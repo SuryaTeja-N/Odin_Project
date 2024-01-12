b66420af317ccdc78f1a40db9f4c8d89d706dc45
SuryaTeja-N <neerukattusurya1@gmail.com>
2024-01-12 10:13:37 +0000

    added comments for js file for animation css excerise

--- css-exercises/animation/03-dropdown-menu/script.js
+++ css-exercises/animation/03-dropdown-menu/script.js
@@ -3,13 +3,15 @@ const menuTitle = document.querySelector(".menu-title");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
 menuTitle.addEventListener("click", (e) => {
-  if (e.target === e.currentTarget) {
+  // currentTarget is the element that the event listener is attached to
+  // target is the element that triggered the event (by the user clicking on it)
+  if (e.target === e.currentTarget) { //when click is on the menu title
     dropdownMenu.classList.toggle("visible");
   }  
 })
 
 window.addEventListener("click", (e) => {
-  if (!dropdownContainer.contains(e.target)) {
+  if (!dropdownContainer.contains(e.target)) { //when click is not on the dropdown menu
     dropdownMenu.classList.remove("visible")
   }
 })
